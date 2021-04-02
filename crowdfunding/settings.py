8656e75ff823f184ca7b5136a61897616d9ecea7
Hossam Khalil <hossamkhalil001@gmail.com>
2021-04-02 20:40:23 +0200

    Feature(auth): created custom user auth model

--- crowdfunding/settings.py
+++ crowdfunding/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'campaign',
 ]
 
+AUTH_USER_MODEL = 'user.User'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
