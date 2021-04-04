80dc7c809f70485aa8998a949303c7f80f42c4d6
Aya <ayahamedabdulaziz@gmail.com>
2021-04-04 18:13:16 +0200

    bug(show-campaign): modify typo in urls.

--- crowdfunding/urls.py
+++ crowdfunding/urls.py
@@ -23,6 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authen.urls')),
     path('social-auth/', include('social_django.urls', namespace="social")),
-]
     path('campaign/', include('campaign.urls')),
     ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
