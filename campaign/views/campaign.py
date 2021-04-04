9fbc10e5df839377b3f897b29adac2b8b7e43bf3
Aya <ayahamedabdulaziz@gmail.com>
2021-04-04 18:17:09 +0200

    feature(campaign-description): retrieve campaign information into the view

--- campaign/views/campaign.py
+++ campaign/views/campaign.py
@@ -6,4 +6,13 @@ from ..models import Campaign, CampaignImage, Rating
 
 
 def show(request, campaign_id):
-    return render(request, 'campaign/show.html')
+    campaign = get_object_or_404(Campaign, id=campaign_id)
+    images = campaign.images.all()
+    average_rating = campaign.ratings.all().aggregate(Avg('value'))['value__avg']
+    if average_rating is None:
+        average_rating = 0
+    donations = campaign.donations.all().aggregate(Sum('amount'))['amount__sum']
+    if donations is None:
+        donations = 0
+    tags = campaign.tags.all()
+    return render(request, 'campaign/show.html' , {'campaign_info' : campaign ,'images':images,'rating':average_rating*25 , 'tags':tags , 'donations':donations})
