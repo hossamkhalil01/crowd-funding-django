c1015f5bb08887754bf678fdfb0f2f5aa23b245b
Hossam Khalil <hossamkhalil001@gmail.com>
2021-04-07 00:08:27 +0200

    Feature(rate-campaign): added user rating to campaign

--- campaign/urls.py
+++ campaign/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
-from .views import campaign, donation
+from .views import campaign, donation, rating
 
 urlpatterns = [
     path('show/<int:campaign_id>', campaign.show, name='campaign_show'),
     path('<int:campaign_id>/donate', donation.donate, name='campaign_donate'),
+    path('<int:campaign_id>/rate', rating.rate, name='campaign_rate'),
 ]
