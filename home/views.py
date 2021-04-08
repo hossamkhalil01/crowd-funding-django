95f405cabd9224739a071a4fad74b21b11d46382
AhmedAtef3 <ahmaadatef3@gmail.com>
2021-04-08 21:39:09 +0200

    feature(sidbar-categories): update side bar with categories

--- home/views.py
+++ home/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render;
-from campaign.models import Campaign;
+from campaign.models import Campaign, Category;
 from django.db.models import Avg;
 
 # Create your views here.
 def index(request):
   highest_rated_campaigns = Campaign.objects.annotate(avg_rate=Avg('ratings__value')).order_by('-avg_rate')[:5]
   latest_campaigns = Campaign.objects.order_by('-creation_date')[:5]
-  print("********************************")
-  print(latest_campaigns)
+  categories = Category.objects.all()
   return render(request, 'home/index.html', {'highest_rated_campaigns': highest_rated_campaigns, 
-  'latest_campaigns': latest_campaigns})
+  'latest_campaigns': latest_campaigns, 'categories': categories})
+
+def category(request, categoty_id):
+  return render(request, 'home/category.html')
