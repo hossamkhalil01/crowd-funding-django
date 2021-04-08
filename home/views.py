e7bbc86725f128e127ba96ac1ddef782e2040577
AhmedAtef3 <ahmaadatef3@gmail.com>
2021-04-08 13:51:59 +0200

    feature(home-page-latest-campaigns-section): completed latest section part

--- home/views.py
+++ home/views.py
@@ -4,5 +4,9 @@ from django.db.models import Avg;
 
 # Create your views here.
 def index(request):
-  campaigns = Campaign.objects.annotate(avg_rate=Avg('ratings__value')).order_by('-avg_rate')[:5]
-  return render(request, 'home/index.html', {'campaigns': campaigns})
+  highest_rated_campaigns = Campaign.objects.annotate(avg_rate=Avg('ratings__value')).order_by('-avg_rate')[:5]
+  latest_campaigns = Campaign.objects.order_by('-creation_date')[:5]
+  print("********************************")
+  print(latest_campaigns)
+  return render(request, 'home/index.html', {'highest_rated_campaigns': highest_rated_campaigns, 
+  'latest_campaigns': latest_campaigns})
